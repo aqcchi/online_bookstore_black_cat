3de9cc7c3251de35cc1549758eb8a94a03b6ed3a
Vaya Line <134380865+aqcchi@users.noreply.github.com>
2024-12-08 15:49:06 +0200

    fixed: custom css

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,20 +5,16 @@ from .models import Profile
 class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
-        exclude = ['user']
+        exclude = ['user', 'email']
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)  # Get the user instance
+        user = kwargs.pop('user', None)  # getting user instance
         super().__init__(*args, **kwargs)
-        if user:
-            self.fields['email'].initial = user.email  # Pre-fill email field with the user's current email
 
     def save(self, commit=True):
-        profile = super().save(commit=False)  # Get the profile instance but don't save yet
-        profile.user.email = self.cleaned_data['email']  # Update the associated user's email field
+        profile = super().save(commit=False)  # get profile instance but no save yet
 
         if commit:
-            profile.user.save()  # Save the updated User instance with the new email
-            profile.save()  # Save the Profile instance
+            profile.save()
 
-        return profile  # Return the updated Profile instance
+        return profile
