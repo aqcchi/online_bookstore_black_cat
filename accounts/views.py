953c0950aef375b308c9ea9e7834ff0227a42ee8
Vaya Line <134380865+aqcchi@users.noreply.github.com>
2024-12-09 11:55:38 +0200

    added: required email field for signup

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,12 @@ from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, DetailView, DeleteView, UpdateView
 
-from accounts.forms import ProfileEditForm
+from accounts.forms import ProfileEditForm, CustomUserCreationForm
 from accounts.models import Profile
 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
 
