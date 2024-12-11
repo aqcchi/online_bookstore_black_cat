82aaae073ab3f2de4ec7bdbb9ccfec6c04d155a1
Vaya Line <134380865+aqcchi@users.noreply.github.com>
2024-12-11 16:49:39 +0200

    fixed: code redundancy

--- authors/forms.py
+++ authors/forms.py
@@ -2,23 +2,20 @@ from django import forms
 from authors.models import Author
 
 
-class AuthorAddForm(forms.ModelForm):
-    class Meta:
-        model = Author
-        fields = '__all__'
-
+class BaseAuthorForm(forms.ModelForm):
     picture = forms.URLField(
         widget=forms.TextInput(attrs={'placeholder': "Enter URL with the author's image"}),
         required=False,
     )
 
-
-class AuthorEditForm(forms.ModelForm):
     class Meta:
         model = Author
         fields = '__all__'
 
-    picture = forms.URLField(
-        widget=forms.TextInput(attrs={'placeholder': "Enter URL with the author's image"}),
-        required=False,
-    )
+
+class AuthorAddForm(BaseAuthorForm):
+    pass
+
+
+class AuthorEditForm(BaseAuthorForm):
+    pass
